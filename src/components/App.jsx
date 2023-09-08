cc0a54995d9a0b67fa1d70e377255879f9e6777a
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-09-09 00:52:31 +0300

    start2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      
-    </div>
-  );
-};
+import Counter from './Counter/Counter';
+import Header from './Header/Header';
+import Modal from './Modal/Modal';
+import { Component } from 'react';
+import TodoList from './TodoList/TodoList';
+
+class App extends Component {
+  state = { isShowModal: false };
+
+  toggleModal = () => {
+    this.setState(prev => ({ isShowModal: !prev.isShowModal }));
+  };
+
+  render() {
+    return (
+      <>
+        <Header toggleModal={this.toggleModal} />
+        <Counter />
+        {this.state.isShowModal && (<Modal toggleModal={this.toggleModal}>Text for Modal</Modal>
+        )}
+
+        <TodoList/>
+      </>
+    );
+  }
+}
+
+export default App;
+// export const App = () => {
+//   return (
+//     <div
+//     style={{
+//       height: '100vh',
+//       display: 'flex',
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//       fontSize: 40,
+//       color: '#010101',
+//     }}
+//     >
+//       <Counter />
+//       <Header />
+//       <Modal></Modal>
+//     </div>
+//   );
+// };
